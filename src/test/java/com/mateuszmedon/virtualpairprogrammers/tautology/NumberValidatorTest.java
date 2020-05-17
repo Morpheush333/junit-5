b7cf28947ee2ecedd3d9ad6c9039ad90d3b99f93
Mateusz Medon <mateuszmedon1@gmail.com>
2020-05-17 13:12:39 +0100

    NumberValidator example

---
+++ src/test/java/com/mateuszmedon/virtualpairprogrammers/tautology/NumberValidatorTest.java
@@ -0,0 +1,37 @@
+package com.mateuszmedon.virtualpairprogrammers.tautology;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class NumberValidatorTest {
+
+    NumberValidator validator;
+
+    @BeforeEach
+    void setUp(){
+        this.validator = new NumberValidator();
+    }
+
+    @Test
+    public void should_ReturnTrue_When_NumberIsPrime() {
+
+        Integer[] numbers = {23,61,79};
+
+        for(int i = 0; i< numbers.length; i++){
+            assertTrue(validator.isItPrime(numbers[i]));
+        }
+    }
+
+    @ParameterizedTest(name = "number = {0}")
+    @CsvSource(value = {"22", "63", "81", "25", "207"})
+    public void should_ReturnFalse_When_NumberIsNotPrime(Integer number) {
+        assertFalse(validator.isItPrime(number));
+    }
+
+
+
+}
