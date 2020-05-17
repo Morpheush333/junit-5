7ca8f70e60ba1fe54907a6a306391405d01be49c
Mateusz Medon <mateuszmedon1@gmail.com>
2020-05-17 12:33:44 +0100

    addon a new test cases, refactoring the code

--- src/main/java/com/mateuszmedon/isbntools/StockManager.java
+++ src/main/java/com/mateuszmedon/isbntools/StockManager.java
@@ -2,14 +2,20 @@ package com.mateuszmedon.isbntools;
 
 public class StockManager {
 
-    ExternalISBNDataService service;
+    ExternalISBNDataService databaseService;
+    ExternalISBNDataService webService;
 
-    public void setService(ExternalISBNDataService service) {
-        this.service = service;
+    public void setDatabaseService(ExternalISBNDataService databaseService) {
+        this.databaseService = databaseService;
+    }
+
+    public void setWebService(ExternalISBNDataService webService) {
+        this.webService = webService;
     }
 
     public String getLocatorCode(String isbn) {
-        Book book = service.lookUp(isbn);
+        Book book = databaseService.lookUp(isbn);
+        if (book == null) book = webService.lookUp(isbn);
         StringBuilder sb = new StringBuilder();
         sb.append(isbn.substring(isbn.length()-4));
         sb.append(book.getAuthor().substring(0,1));
--- src/test/java/com/mateuszmedon/isbntools/StockManagerTest.java
+++ src/test/java/com/mateuszmedon/isbntools/StockManagerTest.java
@@ -1,28 +1,65 @@
 package com.mateuszmedon.isbntools;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
 
 
 class StockManagerTest {
 
+    ExternalISBNDataService databaseService;
+    ExternalISBNDataService webService;
+
+    StockManager stockManager;
+
+    @BeforeEach
+    void setUp() {
+        this.stockManager = new StockManager();
+        this.databaseService = mock(ExternalISBNDataService.class);
+        this.webService = mock(ExternalISBNDataService.class);
+        stockManager.setWebService(databaseService);
+        stockManager.setDatabaseService(webService);
+    }
+
+
     @Test
-    void should_ReturnCorrectLocatorCode_When_IsCall(){
+    void should_ReturnCorrectLocatorCode_When_DataExistInWebService() {
 
-        ExternalISBNDataService testService = new ExternalISBNDataService() {
-            @Override
-            public Book lookUp(String isbn) {
-                return new Book(isbn, "The Witcher: Blood of Elves", "Andrzej Sapkowski");
-            }
-        };
-
-        StockManager stockManager = new StockManager();
-        stockManager.setService(testService);
+        when(webService.lookUp("1473231078")).thenReturn(new Book("1473231078", "The Witcher: Blood of Elves", "Andrzej Sapkowski"));
+        when(databaseService.lookUp("1473231078")).thenReturn(null);
 
         String isbn = "1473231078";
         String locatorCode = stockManager.getLocatorCode(isbn);
 
         assertEquals("1078A5", locatorCode);
     }
+
+    @Test
+    void should_ReturnCorrectLocatorCodeFromDatabase_When_DataIsPresent() {
+
+        when(databaseService.lookUp("1473231078")).thenReturn(new Book("1473231078", "abc", "abc"));
+
+        String isbn = "1473231078";
+        String locatorCode = stockManager.getLocatorCode(isbn);
+
+        verify(databaseService).lookUp("1473231078");
+        verify(webService, never()).lookUp(anyString());
+    }
+
+    @Test
+    void should_ReturnCorrectLocatorCodeFromWebService_When_DataIsPresent() {
+
+        when(databaseService.lookUp("1473231078")).thenReturn(null);
+        when(webService.lookUp("1473231078")).thenReturn(new Book("1473231078", "abc", "abc"));
+
+        String isbn = "1473231078";
+        String locatorCode = stockManager.getLocatorCode(isbn);
+
+        verify(databaseService).lookUp("1473231078");
+        verify(webService).lookUp("1473231078");
+    }
+
+
 }
